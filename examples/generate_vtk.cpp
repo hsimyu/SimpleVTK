e7a4eb5eefdae192c1700feef342f2f7337467fc
hsimyu <samisigena.sazanami@gmail.com>
2017-09-13 18:22:36 +0900

    Updated a simple output example.

--- examples/generate_vtk.cpp
+++ examples/generate_vtk.cpp
@@ -3,54 +3,28 @@
 int main() {
     SimpleVTK gen;
 
-    const int nx = 5;
-    const int ny = 3;
-
+    std::vector<float> values{
+        0, 1, 2, 3, 4, 5, 6, 7, 8,
+        9, 10, 11, 12, 13, 14, 15, 16,
+        17, 18, 19, 20, 21, 22, 23
+    };
     gen.beginVTK("ImageData");
-    std::cout << gen.getRawString() << std::endl;
+        gen.beginContent();
+        gen.setWholeExtent(0, 2, 0, 2, 0, 2);
+        gen.setOrigin(0.0, 0.0, 0.0);
+        gen.setSpacing(1.0, 1.0, 1.0);
+            gen.beginPiece();
+            gen.setExtent(0, 2, 0, 2, 0, 2);
+                gen.beginPointData();
+                gen.setScalars("potential");
+                    gen.beginDataArray("potential", "Float32", "ascii");
+                        gen.addVector(values);
+                    gen.endDataArray();
+                gen.endPointData();
+            gen.endPiece();
+        gen.endContent();
+    gen.endVTK();
     gen.generate("generate_vtk");
-    // gen.beginDomain("Domain1");
-    //     gen.beginGrid("Grid1");
-    //         gen.beginStructuredTopology("Topo1", "2DCoRectMesh");
-    //         gen.setNumberOfElements(nx, ny);
-    //         gen.endStructuredTopology();
-
-    //         gen.beginGeometory("Geom1", "ORIGIN_DXDY");
-    //         gen.setDimensions(nx, ny);
-    //             // Origin
-    //             gen.beginDataItem();
-    //                 gen.setDimensions(2);
-    //                 gen.setFormat("XML");
-    //                 gen.addItem(0.0, 0.0);
-    //             gen.endDataItem();
-
-    //             gen.beginDataItem();
-    //                 gen.setDimensions(2);
-    //                 gen.setFormat("XML");
-    //                 gen.addItem(0.5, 0.5);
-    //             gen.endDataItem();
-    //         gen.endGeometory();
-
-    //         gen.beginAttribute("Attr1");
-    //         gen.setCenter("Node");
-    //             gen.beginDataItem();
-    //                 gen.setDimensions(nx, ny);
-    //                 gen.setFormat("XML");
-
-    //                 // Adding from std::vector
-    //                 std::vector<float> node_values;
-
-    //                 for(int i = 0; i < (nx * ny); ++i) {
-    //                     node_values.emplace_back(i);
-    //                 }
-
-    //                 gen.addVector(node_values);
-    //             gen.endDataItem();
-    //         gen.endAttribute();
-    //     gen.endGrid();
-    // gen.endDomain();
-
-    // gen.generate("generate_xdmf.xmf");
 
     return 0;
 }
